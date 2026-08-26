99f822c36083203e3822fc7124d3c095f4fcc983
junminlee2004 <junminlee2004@gmail.com>
2026-08-26 18:06:43 -0400

    fix build warnings

--- src-gr2/common/logging/backend.cpp
+++ src-gr2/common/logging/backend.cpp
@@ -1,7 +1,9 @@
 // SPDX-FileCopyrightText: Copyright 2014 Citra Emulator Project
 // SPDX-License-Identifier: GPL-2.0-or-later
 
+#ifndef NOMINMAX
 #define NOMINMAX
+#endif
 
 #include <chrono>
 #include <filesystem>
