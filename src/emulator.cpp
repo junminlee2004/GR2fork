ca5763315f6e9658684e35419426f1cb204a92ff
junminlee2004 <junminlee2004@gmail.com>
2026-09-03 02:06:45 -0400

    core: dump the readback batching setting

--- src/emulator.cpp
+++ src/emulator.cpp
@@ -528,6 +528,7 @@ void Emulator::Run(std::filesystem::path file, std::vector<std::string> args,
     LOG_INFO(Config, "GPU imageFastState: {}", EmulatorSettings.IsImageFastState());
     LOG_INFO(Config, "GPU guestCopyLockBatch: {}", EmulatorSettings.IsGuestCopyLockBatch());
     LOG_INFO(Config, "GPU readbackOffloadMode: {}", EmulatorSettings.GetReadbackOffloadMode());
+    LOG_INFO(Config, "GPU readbackBatching: {}", EmulatorSettings.IsReadbackBatchingEnabled());
     LOG_INFO(Config, "GPU readbackLinearImages: {}",
              EmulatorSettings.IsReadbackLinearImagesEnabled());
     LOG_INFO(Config, "GPU directMemoryAccess: {}", EmulatorSettings.IsDirectMemoryAccessEnabled());
